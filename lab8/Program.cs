16abc26c1ec081d2c5ea923917d0442518027798
filde <dmtr.filimonov@yandex.ru>
2024-10-27 16:09:44 +0300

    Add class without functoin

--- lab8/Program.cs
+++ lab8/Program.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace lab8
 {
+    /// <summary>
+    /// Calculate 1/sqrt(x)
+    /// </summary>
+    public class FuncCalc
+    {
+        protected double eps = 0.1;
+
+        public FuncCalc(double e)
+        {
+            eps = Math.Abs(e);
+        }
+
+        public double Eps
+        {
+            get
+            {
+                return eps;
+            }
+
+            set
+            {
+                eps = Math.Abs(value);
+            }
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
